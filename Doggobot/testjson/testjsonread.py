56043ce3bd5cf748a90709a60e8759fe4c9d1a0e
Gutembergson Borges do Nascimento <guto.622@gmail.com>
2021-05-07 17:08:02 -0300

    Update testjsonread.py
    código está imprimindo link com imagem da carta, resta buscar carta especifica no .json

--- Doggobot/testjson/testjsonread.py
+++ Doggobot/testjson/testjsonread.py
@@ -5,5 +5,12 @@ arquivojson = open('set1-pt_br.json', 'r', encoding='utf-8')
 nome = json.load(arquivojson)
 arquivojson.close()
 
-for i in nome:
-    print(i["name"])
+for codigocarta in nome:
+    codigo1 = (codigocarta["cardCode"])
+    
+for setcarta in nome:
+    cardset1 = (setcarta["set"])
+
+codigo = (f'http://dd.b.pvp.net/latest/{cardset1.lower()}/pt_br/img/cards/{codigo1}.png')
+
+print(codigo)
