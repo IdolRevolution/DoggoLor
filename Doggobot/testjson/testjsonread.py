225cbb3d33ccfa5d754a093ca0207f64cf6e2d0f
Gutembergson Borges do Nascimento <guto.622@gmail.com>
2021-05-08 16:52:10 -0300

    Update testjsonread.py
    código traduzido para OO, porém ainda não funcional

--- Doggobot/testjson/testjsonread.py
+++ Doggobot/testjson/testjsonread.py
@@ -1,16 +1,56 @@
 import json
 
 
-arquivojson = open('set1-pt_br.json', 'r', encoding='utf-8')
-nome = json.load(arquivojson)
-arquivojson.close()
+class Carta():
+    def __init__(self, nome_carta):
+        self.nome_carta = nome_carta
+        arquivojson = open('set1-pt_br.json', 'r', encoding='utf-8')
+        cardlist = json.load(arquivojson)
+        arquivojson.close()
 
-for codigocarta in nome:
-    codigo1 = (codigocarta["cardCode"])
-    
-for setcarta in nome:
-    cardset1 = (setcarta["set"])
 
-codigo = (f'http://dd.b.pvp.net/latest/{cardset1.lower()}/pt_br/img/cards/{codigo1}.png')
+        for cardname in cardlist:
+            cardname = (cardname['name'])
+            if cardname == self.nome_carta:
+                carta_buscada = cardname
+                if carta_buscada == True:
+                    carta_buscada = self.nome_carta
 
-print(codigo)
+    def __str__(self):
+        return self.link
+        
+    @property
+    def codigocarta(self):
+        arquivojson = open('set1-pt_br.json', 'r', encoding='utf-8')
+        cardlist = json.load(arquivojson)
+        arquivojson.close()
+        
+
+        for codigocarta in self.nome_carta:
+            codigocarta = (codigocarta["cardCode"])
+            return codigocarta
+
+    @property        
+    def setcarta(self):
+        arquivojson = open('set1-pt_br.json', 'r', encoding='utf-8')
+        cardlist = json.load(arquivojson)
+        arquivojson.close()
+
+
+        for setcarta in self.nome_carta:
+            setcarta = (setcarta["set"])
+            return setcarta
+
+    def link(self):
+        arquivojson = open('set1-pt_br.json', 'r', encoding='utf-8')
+        cardlist = json.load(arquivojson)
+        arquivojson.close()
+        setcarta = self.setcarta()
+        codigocarta = self.codigocarta()
+
+        return (f'http://dd.b.pvp.net/latest/{setcarta.lower()}/pt_br/img/cards/{codigocarta}.png')
+
+
+teste = Carta("Garen")
+teste.link()
+
