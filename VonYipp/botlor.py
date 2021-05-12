9ab1a7b7f50691f52dfe0bfc1526172de9f88384
Gutembergson Borges do Nascimento <guto.622@gmail.com>
2021-05-12 19:33:20 -0300

    Pequenas alterações

--- VonYipp/botlor.py
+++ VonYipp/botlor.py
@@ -6,10 +6,10 @@ client = commands.Bot(command_prefix = ">", case_insensitive = True)
 
 @client.event
 async def on_ready():
-    print("Salve, Salve Yodinha!!!!")
+    print("O Fim da Humanidade é agora!!! MEOOOW")
 
 @client.command()
 async def ola(ctx):
-    await ctx.send(f'Olá! {ctx.author}, quer um biscoito?')
+    await ctx.send(f'Olá {ctx.author}, Humano Insolente...')
 
 client.run(config("DISCORD_TOKEN"))
--- VonYipp/testjson/testjsonread.py
+++ VonYipp/testjson/testjsonread.py
@@ -1,14 +1,17 @@
 import json
 
 
-arquivojson = open('set1-pt_br.json', 'r', encoding='utf-8')
+arquivojson = open('finaljson.json', 'r', encoding='utf-8')
 cardlist = json.load(arquivojson)
 arquivojson.close()
 
-for dicionario in cardlist:
-    if "Garen" in dicionario.values():
-        assets = dicionario["assets"]
-        for item in assets:
-            item = item["gameAbsolutePath"]
-            print(item)
 
+def find_card(carta):
+    for dicionario in cardlist:
+        if carta in dicionario.values():
+            assets = dicionario["assets"]
+            for item in assets:
+                item = item["gameAbsolutePath"]
+                print(item)
+
+find_card("Yasuo")
