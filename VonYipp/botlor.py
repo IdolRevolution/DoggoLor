f4def43daedc3025e8dd32057d74c917d837eb29
Gutembergson Borges do Nascimento <guto.622@gmail.com>
2021-05-15 17:15:26 -0300

    Encontrar Cartas (Find Cards)
    Von Yipp agora já consegue encontrar cartas! porém apenas as que só tem uma palavra, vamos corrigir isso em breve.
    
    Von Yipp can now find cards! but only the ones thac contain just one word, we gonna fix that soon.

--- VonYipp/botlor.py
+++ VonYipp/botlor.py
@@ -1,8 +1,13 @@
 import discord
 from discord.ext import commands
 from decouple import config
+import json
 
 client = commands.Bot(command_prefix = ">", case_insensitive = True)
+
+arquivojson = open('finaljson.json', 'r', encoding='utf-8')
+cardlist = json.load(arquivojson)
+arquivojson.close()
 
 @client.event
 async def on_ready():
@@ -11,5 +16,16 @@ async def on_ready():
 @client.command()
 async def ola(ctx):
     await ctx.send(f'Olá {ctx.author}, Humano Insolente...')
+
+@client.command()
+async def carta(ctx, carta):
+    carta = (carta.capitalize())
+    for dicionario in cardlist:
+        if carta in dicionario.values():
+            assets = dicionario["assets"]
+            for item in assets:
+                item = item["gameAbsolutePath"]
+                await ctx.send(item)
+
 
 client.run(config("DISCORD_TOKEN"))
